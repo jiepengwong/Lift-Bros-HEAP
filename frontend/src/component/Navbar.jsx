e3556e14be1c98ce637ccc3f0ad5eb05b8980b71
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2023-05-27 21:02:12 +0800

    edited and added logic for planner cards

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom';
+
 function Navbar() {
 
-    const button =                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+    const button = <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
 
-    const [navbar, setNavbar] = useState(['Planner', 'Gyms', 'Profile'])
+    const [navbar, setNavbar] = useState(['Home','Planner'])
     const [menuOpen, setMenuOpen] = useState(false);
     const [signIn, setSignIn] = useState(false);
 
@@ -15,7 +17,7 @@ function Navbar() {
 
 
     return (
-        <div className="fixed w-full h-">
+        <div className="top-0 w-full block">
             <div className="bg-yellow-300 flex justify-between items-center w-full h-20 px-2  2xl:px-16">
 
                 {/* Header/logo */}
@@ -28,10 +30,12 @@ function Navbar() {
                     <ul>
                         {/* Links */}
                         {navbar.map((item) => (
-                            <li className="font-bold cursor-pointer inline-block px-6 py-2 mx-2 rounded-md hover:bg-green-300">{item}</li>
-                        ))}
+                            <li className="font-bold cursor-pointer inline-block px-6 py-2 mx-2 rounded-md hover:bg-green-300">
+                                {/* check  if item is home */}
+                                {item === 'Home' ? <Link to='/'>{item}</Link> : <Link to={`/${item}`}>{item}</Link>}
+                            </li>))}
 
-                       
+
 
 
 
@@ -44,12 +48,12 @@ function Navbar() {
                 <div className="sm:hidden flex justify-between items-center">
                     {/* Hamburger Icon */}
                     <div
-                        className='cursor-pointer' 
+                        className='cursor-pointer'
                         onClick={() => setMenuOpen(!menuOpen)}
                     >
-                    <div className="w-6 h-0.5 bg-black"></div>
-                    <div className="w-6 h-0.5 bg-black my-1"></div>
-                    <div className="w-6 h-0.5 bg-black"></div>
+                        <div className="w-6 h-0.5 bg-black"></div>
+                        <div className="w-6 h-0.5 bg-black my-1"></div>
+                        <div className="w-6 h-0.5 bg-black"></div>
                     </div>
                     {/* Mobile Menu */}
                     <div className={`${!menuOpen ? 'hidden' : 'block'}`}>

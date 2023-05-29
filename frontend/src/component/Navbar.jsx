237a123de0decc56cacd5112d937381dc5b78aca
jiepengwong <johndoe@example.com>
2023-05-30 00:57:12 +0800

    Added animation for mobile + tabs for planner

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
 
     const button = <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
 
-    const [navbar, setNavbar] = useState(['Home','Planner'])
+    const [navbar, setNavbar] = useState(['Home', 'Planner'])
     const [menuOpen, setMenuOpen] = useState(false);
     const [signIn, setSignIn] = useState(false);
 
@@ -14,66 +14,103 @@ function Navbar() {
         setMenuOpen(!menuOpen);
     }
 
+    const handleMenuClick = (item) => {
+        // you can close the menu after clicking an item
+        setMenuOpen(false);
+
+    };
+
 
 
     return (
         <div className="top-0 w-full block">
-            <div className="bg-yellow-300 flex justify-between items-center w-full h-20 px-2  2xl:px-16">
-
+            <div className="bg-yellow-300 flex justify-between items-center w-full h-20 px-2 2xl:px-16">
                 {/* Header/logo */}
-                <div className=''>
+                <div className="">
                     <p className="font-bold text-2xl">Lift bros 🦾</p>
                 </div>
 
                 {/* Navbar desktop */}
-                <div className='hidden sm:block'>
+                <div className="hidden sm:block">
                     <ul>
                         {/* Links */}
                         {navbar.map((item) => (
-                            <li className="font-bold cursor-pointer inline-block px-6 py-2 mx-2 rounded-md hover:bg-green-300">
-                                {/* check  if item is home */}
-                                {item === 'Home' ? <Link to='/'>{item}</Link> : <Link to={`/${item}`}>{item}</Link>}
-                            </li>))}
-
-
-
-
-
-
-
+                            <li
+                                key={item}
+                                className="font-bold cursor-pointer inline-block px-6 py-2 mx-2 rounded-md hover:bg-green-300"
+                            >
+                                {/* Check if item is home */}
+                                {item === 'Home' ? (
+                                    <Link to="/">{item}</Link>
+                                ) : (
+                                    <Link to={`/${item}`}>{item}</Link>
+                                )}
+                            </li>
+                        ))}
                     </ul>
                 </div>
 
                 {/* Navbar mobile */}
-                <div className="sm:hidden flex justify-between items-center">
+                <div className="sm:hidden flex flex-1 justify-end items-center">
                     {/* Hamburger Icon */}
                     <div
-                        className='cursor-pointer'
-                        onClick={() => setMenuOpen(!menuOpen)}
+                        className={`cursor-pointer flex flex-col justify-center items-center w-6 h-6`}
+                        onClick={() => setMenuOpen((menuOpen) => !menuOpen)}
                     >
-                        <div className="w-6 h-0.5 bg-black"></div>
-                        <div className="w-6 h-0.5 bg-black my-1"></div>
-                        <div className="w-6 h-0.5 bg-black"></div>
-                    </div>
-                    {/* Mobile Menu */}
-                    <div className={`${!menuOpen ? 'hidden' : 'block'}`}>
-                        <div className="absolute top-full left-0 bg-white font-bold w-full p-4 shadow">
-                            {/* Mobile Menu Content */}
-                            <ul>
-                                {navbar.map((item) => (
-                                    <li className="cursor-pointer py-2 rounded-md hover:bg-green-300" key={item}>
-                                        {item}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                        {/* Hamburger Lines */}
+                        <div
+                            className={`w-6 h-0.5 bg-black transition-all duration-300 transform ${menuOpen ? 'rotate-45 translate-y-1.5' : ''
+                                }`}
+                        ></div>
+                        <div
+                            className={`w-6 h-0.5 bg-black my-1 transition-all duration-300 opacity ${menuOpen ? 'opacity-0' : ''
+                                }`}
+                        ></div>
+                        <div
+                            className={`w-6 h-0.5 bg-black transition-all duration-300 transform ${menuOpen ? '-rotate-45 -translate-y-1.5' : ''
+                                }`}
+                        ></div>
                     </div>
                 </div>
 
 
             </div>
 
+            {/* Mobile Menu, outside of the flex*/}
+            <div
+                className={`${menuOpen ? 'flex flex-col' : 'hidden'
+                    } z-10 bg-white shadow-lg relative rounded-xl sidebar`}
+                style={{
+                    backgroundColor: menuOpen ? '#f0f0f0' : 'white',
+                    borderRadius: menuOpen ? '4px' : '0',
+                }}
+            >
+                {/* Caret */}
+                {/* <div className="absolute top-0 right-0 w-9 h-9 transform rotate-45 bg-gray-200"> */}
+                {/* Empty div to hide the caret on hover */}
+                {/* <div className="w-full h-full bg-white transition-opacity duration-300 hover:opacity-0"></div>
+                </div> */}
 
+                {/* Mobile Menu Content */}
+                <ul>
+                    {/* Links */}
+                    {navbar.map((item, index) => (
+                        <li
+                            key={item}
+                            className={`z-10 font-bold cursor-pointer block py-2 mx-2 rounded-md ${index === navbar.length - 1 ? 'last-item' : ''
+                                }`}
+                            onClick={() => handleMenuClick(item)}
+                        >
+                            {/* Check if item is home */}
+                            {item === 'Home' ? (
+                                <Link to="/">{item}</Link>
+                            ) : (
+                                <Link to={`/${item}`}>{item}</Link>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
         </div>
     )
--- frontend/src/pages/Planner.js
+++ frontend/src/pages/Planner.js
@@ -1,20 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 // Components
 import CardPlanner from '../component/CardPlanner';
 function Planner() {
+
+  // Active tab state
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabChange = (index) => {
+    setActiveTab(index);
+  };
+
+  // Need to account for content display based on the active tab
+
+
+  // Database data 
+
   return (
     <div>
       <div>
         {/* Planner page */}
 
 
-        <h1 class="text-2xl lg:text-4xl font-bold text-center m-10 p-2">Welcome to the Planner Page</h1>
+        <h1 class="text-xl sm:text-lg lg:text-4xl font-bold text-center m-10 p-2">Welcome to the Planner Page</h1>
 
+        {/* Tab bar to filter between my Routines, favourites and other people routines */}
+        <div className="flex justify-center mb-11">
+          <nav className="flex space-x-10">
+            <button
+              className={`text-xl text-gray-500 hover:text-gray-800 focus:outline-none ${activeTab === 0 ? 'border-b-2 border-blue-500' : ''
+                }`}
+              onClick={() => handleTabChange(0)}
+            >
+              My Routines
+            </button>
+            <button
+              className={`text-xl text-gray-500 hover:text-gray-800 focus:outline-none ${activeTab === 1 ? 'border-b-2 border-blue-500' : ''
+                }`}
+              onClick={() => handleTabChange(1)}
+            >
+              My Favourites
+            </button>
+            <button
+              className={` text-xl text-gray-500 hover:text-gray-800 focus:outline-none ${activeTab === 2 ? 'border-b-2 border-blue-500' : ''
+                }`}
+              onClick={() => handleTabChange(2)}
+            >
+              Other Routines
+            </button>
+          </nav>
+        </div>
 
-      {/* Grid act as the container here */}
+        {/* Grid act as the container here */}
         <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-10 place-items-center">
           {/* CardPlanner Components Hardcoded */}
           <CardPlanner />

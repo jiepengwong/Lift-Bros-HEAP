d5a1d54f1c1bfd8ce4e27423aefe8d207ec28053
BaoShinYeo <baoshin.yeo.2019@accountancy.smu.edu.sg>
2023-06-14 00:12:41 +0800

    Removed Daemon and cleaned up code

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,3 @@
 wait-for "mysql:3306" -- "$@"
 
-# Watch your .go files and invoke go build if the files changed.
-CompileDaemon -build="go build main.go"  -command="./main"
-# CompileDaemon -build="go build -o /build/app"  -command="/build/app"
-# CompileDaemon -command="./app"
-# go run main.go
+go run main.go

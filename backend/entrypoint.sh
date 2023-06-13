ecf803961f495f3ab46a01768f0acb21608283f6
BaoShinYeo <baoshin.yeo.2019@accountancy.smu.edu.sg>
2023-06-14 00:07:30 +0800

    Modify Daemon

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,7 @@
 wait-for "mysql:3306" -- "$@"
 
 # Watch your .go files and invoke go build if the files changed.
-CompileDaemon --build="go build -o main main.go"  --command=./main
+CompileDaemon -build="go build main.go"  -command="./main"
+# CompileDaemon -build="go build -o /build/app"  -command="/build/app"
+# CompileDaemon -command="./app"
 # go run main.go

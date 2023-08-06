dd7a7b3ae52556085fd273dd3875e960565b2a94
BaoShinYeo <baoshin.yeo.2019@accountancy.smu.edu.sg>
2023-08-06 20:39:52 +0800

    Axios configuration

--- frontend/src/axios/baseAxios.js
+++ frontend/src/axios/baseAxios.js
@@ -1,12 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Create a base Axios instance
 const baseAxios = axios.create({
-  baseURL: 'backe-LoadB-JAKS2OQHCEQE-9e78a5db6d3187e4.elb.ap-southeast-1.amazonaws.com:8080', 
+  baseURL:
+    "http://backe-LoadB-10MATTFY7PXAU-451efe42897ddd4d.elb.ap-southeast-1.amazonaws.com:8080",
   timeout: 10000, // Set a default timeout for all requests (optional)
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json', // Set default headers for all requests (optional)
+    "Content-Type": "application/json", // Set default headers for all requests (optional)
     // You can also set any other default headers you need
   },
 });

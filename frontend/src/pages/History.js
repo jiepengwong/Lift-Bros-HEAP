768e2996288dc712c32e029ba18a05f741ca0f46
seanleong97 <seanleong1997@hotmail.com>
2023-08-06 15:11:35 +0800

    History page updates

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -6,16 +6,7 @@ import CardHistory from '../component/CardHistory';
 import Searchbar from '../component/Searchbar';
 
 const History = () => {
-  const[histories, setHistories] = useState([
-    // {date: "15/6/2023", routinename: "RoutineName1", exercises: ["Name1", "Name2", "Name3"], duration: "2 Hours", calories: "240KJ"},
-    // {date: "28/12/1997", routinename: "RoutineName2", exercises: ["Name1", "Name2", "Name3"], duration: "2 Hours", calories: "240KJ"},
-    // {date: "12/5/2020", routinename: "RoutineName3", exercises: ["Name1", "Name2", "Name3"], duration: "2 Hours", calories: "240KJ"},
-    // {date: "30/5/2004", routinename: "RoutineName4", exercises: ["Name1", "Name2", "Name3"], duration: "2 Hours", calories: "240KJ"},
-    // {date: "5/7/2008", routinename: "RoutineName5", exercises: ["Name1", "Name2", "Name3"], duration: "2 Hours", calories: "240KJ"},
-    // {date: "6/8/2018", routinename: "RoutineName6", exercises: ["Name1", "Name2", "Name3"], duration: "2 Hours", calories: "240KJ"},
-    // {date: "8/3/2015", routinename: "RoutineName7", exercises: ["Name1", "Name2", "Name3"], duration: "2 Hours", calories: "240KJ"},
-    // {date: "8/3/2015", routinename: "RoutineName8", exercises: ["Name1", "Name2", "Name3"], duration: "2 Hours", calories: "240KJ"},
-  ]);
+  const[histories, setHistories] = useState([])
 
   useEffect(async () => {
     try {

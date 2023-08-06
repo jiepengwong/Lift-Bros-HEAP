c0d6044b50fdcabd60ba78f0c77c61f34bb6cefa
seanleong97 <seanleong1997@hotmail.com>
2023-08-06 12:10:37 +0800

    Cardhistory

---
+++ frontend/src/component/CardHistory.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+
+const CardHistory = ({histories}) => {
+    console.log(histories)
+
+    return (
+        <div className="grid md:grid-cols-2 gap-10 lg:grid-cols-4 gap-10 place-items-center py-10">
+            {histories.map((history) =>(
+                <div className="w-full block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 className="mb-2 text-lg font-bold tracking-tight text-left text-gray-900 dark:text-white">{history.dateTimeCompleted.slice(0,10)}</h5>
+                    <h5 className="mb-2 text-lg font-bold tracking-tight text-left text-gray-900 dark:text-white">{history.routineName}</h5>
+                </a>
+                <p className="mb-3 font-normal text-start text-gray-700 dark:text-gray-200">Exercises Done:</p>
+                <ul className ="list-disc space-x-2 grid grid-cols-1">
+                    {history.completedExercises.map((exercise) => (
+
+                        <li className=" mb-3 font-normal text-start text-gray-700 dark:text-gray-200 ml-2">{exercise.exerciseName}</li>
+
+                    )
+                    
+                    
+                    ) }
+                </ul>
+                <p className="mb-3 font-normal text-start text-gray-700 dark:text-gray-400">Duration: {history.routineDuration}</p>
+                <p className="mb-3 font-normal text-start text-gray-700 dark:text-gray-400">Calories Burnt: {history.caloriesBurned}</p>
+                <div className="flex flex-row">
+                </div>
+                </div>
+            ))}
+        </div>
+        
+        )
+}
+
+export default CardHistory

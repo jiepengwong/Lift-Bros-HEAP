e7f4f6a61c61a147610392cce843ce5f81419058
jiepengwong <johndoe@example.com>
2023-08-08 02:58:40 +0800

    Change loading animations

--- frontend/src/component/Loading.jsx
+++ frontend/src/component/Loading.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 const Loading = () => {
     return (
       <div className="loading-overlay">
-        <div className="loader"></div>
-        <h2 className="loading-text">Loading...</h2>
-        <p className="loading-description">This may take a few seconds, please don't close this page.</p>
+        <div className="loader-container">
+          <div className="barbell">
+            <div className="bar">
+              <div className="plate plate-top"></div>
+              <div className="plate plate-bottom"></div>
+            </div>
+          </div>
+        </div>
+        <h2 className="loading-text">Getting Fit...</h2>
+        <p className="loading-description">Stay strong, your data is on its way!</p>
       </div>
     );
   };
-
 export default Loading

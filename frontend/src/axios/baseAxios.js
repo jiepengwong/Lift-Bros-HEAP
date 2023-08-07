649577cbb579ed404f99131efb7905581beb04a4
Wong Jie Peng <76239879+jiepengwong@users.noreply.github.com>
2023-08-07 13:34:24 +0800

    deployment testing branch.

--- frontend/src/axios/baseAxios.js
+++ frontend/src/axios/baseAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a base Axios instance
 const baseAxios = axios.create({
-  baseURL: 'backe-LoadB-JAKS2OQHCEQE-9e78a5db6d3187e4.elb.ap-southeast-1.amazonaws.com:8080', 
+  baseURL: 'http://localhost:8080', 
   timeout: 10000, // Set a default timeout for all requests (optional)
   withCredentials: true,
   headers: {

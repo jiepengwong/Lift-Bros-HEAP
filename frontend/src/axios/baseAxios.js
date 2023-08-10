e0f905444c42b13d9a7a2cb7e821800a7b6db7ad
BaoShinYeo <baoshin.yeo.2019@accountancy.smu.edu.sg>
2023-08-11 01:01:03 +0800

    Stringify JSON to reflect error

--- frontend/src/axios/baseAxios.js
+++ frontend/src/axios/baseAxios.js
@@ -26,13 +26,16 @@ baseAxios.interceptors.request.use((config) => {
 baseAxios.interceptors.response.use(
   (response) => response,
   (error) => {
-
-    console.log(error)
+    console.log(error);
     if (error.response) {
-      if (error.response.status === 401 || error.response.status === 500 || error.response.code === "ERR_NETWORK") {
+      if (
+        error.response.status === 401 ||
+        error.response.status === 500 ||
+        error.response.code === "ERR_NETWORK"
+      ) {
         // Handle unauthorized or internal server errors
         // Example: logOutUser();
-        console.log("hi i am in this block base axios ")
+        console.log("hi i am in this block base axios ");
         if (localStorage.getItem("token")) {
           localStorage.removeItem("token");
         }
@@ -40,22 +43,23 @@ baseAxios.interceptors.response.use(
           localStorage.removeItem("username");
         }
         if (document.cookie) {
-          document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+          document.cookie =
+            "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         }
         // You can also redirect the user to a login page or display an error message
         // Example: redirectToLoginPage();
-      } 
+      }
       Swal.fire({
         title: "Error!",
-        text: "Something went wrong!" + error.response,
+        text:
+          "Something went wrong!" + JSON.stringify(error.response.data.error),
         icon: "error",
         confirmButtonText: "Cool",
       });
-    }
-    else {
-      console.log(error.code =="ERR_NETWORK", "In else clause of base axios")
+    } else {
+      console.log(error.code == "ERR_NETWORK", "In else clause of base axios");
 
-      if (error.code =="ERR_NETWORK") {
+      if (error.code == "ERR_NETWORK") {
         if (localStorage.getItem("token")) {
           localStorage.removeItem("token");
         }
@@ -63,7 +67,8 @@ baseAxios.interceptors.response.use(
           localStorage.removeItem("username");
         }
         if (document.cookie) {
-          document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+          document.cookie =
+            "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         }
 
         Swal.fire({
@@ -76,9 +81,6 @@ baseAxios.interceptors.response.use(
             window.location.href = "/login"; // Replace with the appropriate route
           }
         });
-
-
-
       }
     }
     return Promise.reject(error);

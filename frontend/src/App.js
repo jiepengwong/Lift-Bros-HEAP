578918ab2e18f0595827cd44ba8ad9fb30395410
jiepengwong <johndoe@example.com>
2023-08-10 18:26:29 +0800

    react helmet and favicon title changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,10 +18,10 @@ import Endpage from "./pages/Endpage";
 import LoginTest from "./pages/LoginTest";
 import RequireAuth from "./utils/RequireAuth";
 // import useAuth from "./utils/useAuth";
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, useNavigate, Link } from "react-router-dom";
 import { startGlobalTimer, clearGlobalTimer } from "./utils/GlobalTimer"; // Import the new utility
-
+import { Helmet } from 'react-helmet';
 function App() {
   const navigate = useNavigate();
 
@@ -34,6 +34,13 @@ function App() {
   );
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Lift Bro</title>
+        <link rel="canonical" href="https://liftbro.netlify.app/" />
+        <meta name="description" content="Start your gym routine today with LiftBro" />
+
+      </Helmet>
       {shouldRenderNavbar && <Navbar />}
       <div>
         <Routes>
@@ -41,14 +48,14 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           {/* Protected Routes */}
-            <Route element={<RequireAuth />}>
-              <Route path="/" element={<Homepage />} exact />
-              <Route path="/routine" element={<Planner />} />
-              <Route path="/createRoutine" element={<CreateRoutine />} />
-              <Route path="/end" element={<Endpage />} />
-              <Route path="/during" element={<DuringRoutine />} />
-              <Route path="/history" element={<History />} />
-            </Route>
+          <Route element={<RequireAuth />}>
+            <Route path="/" element={<Homepage />} exact />
+            <Route path="/routine" element={<Planner />} />
+            <Route path="/createRoutine" element={<CreateRoutine />} />
+            <Route path="/end" element={<Endpage />} />
+            <Route path="/during" element={<DuringRoutine />} />
+            <Route path="/history" element={<History />} />
+          </Route>
 
         </Routes>
       </div>

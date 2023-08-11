6a116533e9c6151a162df2cd9fda0849d4bf34cb
BaoShinYeo <baoshin.yeo.2019@accountancy.smu.edu.sg>
2023-08-11 19:00:30 +0800

    Include update for star rating

--- frontend/src/component/StarRating.jsx
+++ frontend/src/component/StarRating.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 
-function StarRating(onChange) {
+function StarRating({ onChange }) {
   const [rating, setRating] = useState(0);
 
   const handleStarClick = (selectedRating) => {
+    console.log("selected", selectedRating);
+    // unselect if already selected
+    if (selectedRating === rating) {
+      setRating(0);
+      onChange(0);
+      return;
+    }
     setRating(selectedRating);
+    onChange(selectedRating);
   };
 
   return (
     <div className="bg-gray-200 rounded-lg p-4">
       <p className="font-bold text-xl">Intensity rating</p>
       {[1, 2, 3, 4, 5].map((value) => (
-        <span
+        <button
           key={value}
           className={`text-gray-400 text-3xl ${
             value <= rating ? "text-yellow-500" : "text-gray-300"
@@ -19,7 +27,7 @@ function StarRating(onChange) {
           onClick={() => handleStarClick(value)}
         >
           ★
-        </span>
+        </button>
       ))}
     </div>
   );
--- frontend/src/pages/Endpage.js
+++ frontend/src/pages/Endpage.js
@@ -1,47 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GreyBox from "../component/GreyBox";
 import StarRating from "../component/StarRating";
 import Notes from "../component/Notes";
 import Button from "../component/Button";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import baseAxios from "../axios/baseAxios";
 
 function Endpage() {
   // grab your completed routine id from local storage
+  const navigate = useNavigate();
   const displayCompletedRoutineId = localStorage.getItem("completedRoutineId");
   const [caloriesBurned, setCaloriesBurned] = useState(0);
   const [routineDuration, setRoutineDuration] = useState("");
   const [routineName, setRoutineName] = useState("");
+  const [intensity, setIntensity] = useState(0);
   // get the completed routine from the backend using completed routine id
-  const response = baseAxios
-    .get(`/completedRoutine/${displayCompletedRoutineId}`, {
-      withCredentials: true,
-    })
-    .then((response) => {
-      const completedRoutineNoStar = response.data.data;
-      console.log("Workout data retrieved", completedRoutineNoStar);
-      // get the routine name, routine duration, and routine calories burned from the backend and assign to variable
-      const routineName = completedRoutineNoStar.routineName;
-      setRoutineName(completedRoutineNoStar.routineName);
-      console.log("rn", routineName);
-
-      const caloriesBurned = completedRoutineNoStar.caloriesBurned;
-      setCaloriesBurned(completedRoutineNoStar.caloriesBurned);
-      console.log("rc", caloriesBurned);
-
-      const routineDuration = completedRoutineNoStar.routineDuration;
-      console.log("rd", routineDuration);
-      setRoutineDuration(completedRoutineNoStar.routineDuration);
-    })
-    .catch((error) => {
-      // Handle any error that occurs during the API call
-      console.error("Error retrieving workout data:", error);
-    });
+  useEffect(() => {
+    baseAxios
+      .get(`/completedRoutine/${displayCompletedRoutineId}`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        const completedRoutineNoStar = response.data.data;
+        console.log("Workout data retrieved", completedRoutineNoStar);
+        // get the routine name, routine duration, and routine calories burned from the backend and assign to variable
+        setRoutineName(completedRoutineNoStar.routineName);
+        setCaloriesBurned(completedRoutineNoStar.caloriesBurned);
+        setRoutineDuration(completedRoutineNoStar.routineDuration);
+      })
+      .catch((error) => {
+        // Handle any error that occurs during the API call
+        console.error("Error retrieving workout data:", error);
+      });
+  }, []);
 
   // create function to update the completed routine intensity
-  const processStar = (starInfo) => {};
+  const processStar = (starInfo) => {
+    setIntensity(starInfo);
+  };
   // use post request to update the completed routine intensity
-
+  const updateRoutineAndNavigate = () => {
+    baseAxios
+      .put(`/completedRoutine/${displayCompletedRoutineId}`, {
+        routineIntensity: intensity,
+      })
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-xl sm:text-lg lg:text-3xl font-bold text-center m-2 p-4">
@@ -67,9 +76,9 @@ function Endpage() {
       </div>
 
       <div className="flex justify-center p-4">
-        <Link to="/">
-          <Button text="Save & back to home" />
-        </Link>
+        {/* <Link to="/"> */}
+        <Button text="Save & back to home" onClick={updateRoutineAndNavigate} />
+        {/* </Link> */}
       </div>
     </div>
   );
